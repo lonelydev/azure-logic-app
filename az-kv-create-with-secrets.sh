f3d1ebe447720bd3d042188b117e40290867ffc4
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-06-16 22:10:26 +0100

    feat(secrets): Key Vault Updated
    - script updated
    - template updated

--- az-kv-create-with-secrets.sh
+++ az-kv-create-with-secrets.sh
@@ -2,22 +2,22 @@
 # A script to create an az KeyVault enabled for template deployment
 
 #Change the values below before running the script
-vaultName="fantastic-vault-name"               #Globally Unique Name of the KeyVault
-vaultLocation="VaultLocation"           #Location of the KeyVault
-resourceGroupName="ResourceGroupName"        #Name of the resource group for the vault
-resourceGroupLocation="ResourceGroupLocation"   #Location of the resource group if it needs to be created
+vaultName="az-lga-sendgrid-vault"               #Globally Unique Name of the KeyVault
+vaultLocation="UK South"           #Location of the KeyVault
+resourceGroupName="AzureLogicApp"        #Name of the resource group for the vault
+resourceGroupLocation="UK South"   #Location of the resource group if it needs to be created
 
 resourceGroupConnVarName="ResourceManagerConnection"
-resourceGroupConnVarValue="ResourceManagerConVal"
+resourceGroupConnVarValue="Pluralsight (d6d0fd8c-aab6-4713-b2f1-c9a0375d687d)"
 
 subscriptionNameVarName="SubscriptionName"
-subscriptionNameVarValue="SubvarValue"
+subscriptionNameVarValue="d6d0fd8c-aab6-4713-b2f1-c9a0375d687d"
 
 resourceGroupVarName="ResourceGroup"
-resourceGroupVarValue="ResGroupVal"
+resourceGroupVarValue="$resourceGroupName"
 
 locationVarName="Location"
-locationVarValue="SecretLocation"
+locationVarValue="$resourceGroupLocation"
 #Login and Select the default subscription if needed
 #az login
 #az account set "subscription name"
@@ -27,8 +27,8 @@ locationVarValue="SecretLocation"
 
 az keyvault create --name "$vaultName" --resource-group "$resourceGroupName" --location "$vaultLocation" --enabled-for-template-deployment true
 
-az keyvault secret set --vault-name "$vaultName" --name "resourceGroupConnVarName" --value "resourceGroupConnVarValue"
-az keyvault secret set --vault-name "$vaultName" --name "subscriptionNameVarName" --value "subscriptionNameVarValue"
-az keyvault secret set --vault-name "$vaultName" --name "resourceGroupVarName" --value "resourceGroupVarValue"
-az keyvault secret set --vault-name "$vaultName" --name "locationVarName" --value "locationVarValue"
+az keyvault secret set --vault-name "$vaultName" --name "$resourceGroupConnVarName" --value "$resourceGroupConnVarValue"
+az keyvault secret set --vault-name "$vaultName" --name "$subscriptionNameVarName" --value "$subscriptionNameVarValue"
+az keyvault secret set --vault-name "$vaultName" --name "$resourceGroupVarName" --value "$resourceGroupVarValue"
+az keyvault secret set --vault-name "$vaultName" --name "$locationVarName" --value "$locationVarValue"
 
